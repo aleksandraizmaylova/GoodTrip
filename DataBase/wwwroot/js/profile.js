2f79a9571bff06cc32b65e355576b7425b2b767b
aleksandraizmaylova <aleksandraizmaylova111@gmail.com>
2026-04-22 12:29:15 +0500

    забыла ветку, ща уберу

---
+++ DataBase/wwwroot/js/profile.js
@@ -0,0 +1,40 @@
+async function configureProfile() {
+    const meResponse = await fetch("/api/me");
+    if (meResponse.ok) {
+        const meResult = await meResponse.json();
+        document.getElementById("username").textContent = `Имя: ${meResult.user.username}`;
+        document.getElementById("email").textContent = `Почта: ${meResult.user.email}`;
+        document.getElementById("loginBtn").style.display = 'none';
+    } else {
+        document.getElementById("username").style.display = 'none';
+        document.getElementById("email").style.display = 'none';
+        document.getElementById("logoutBtn").style.display = 'none';
+    }
+}
+
+function closeProfile() {
+    document.getElementById('profile-panel').classList.remove('open');
+}
+
+function switchProfile(){
+    const profileClassList = document.getElementById('profile-panel').classList;
+    if (profileClassList.contains('open')) {
+        profileClassList.remove('open');
+    } else {
+        profileClassList.add('open');
+    }
+}
+
+document.getElementById("logoutBtn").addEventListener("click", async () => {
+    await fetch("/api/logout", { method: "POST" });
+    window.location.href = "/v3.html";
+});
+
+document.getElementById("loginBtn").addEventListener("click", async () => {
+    await fetch("/api/login", { method: "POST" });
+    window.location.href = "/login.html";
+});
+
+document.getElementById("achievementsBtn").addEventListener("click", async () => {
+    window.location.href = "/achievements.html";
+});
